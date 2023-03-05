6d33d2c2b0e13b855242405204d21520fe7888a4
Joel Storr <joel.storr@ewe.net>
2023-03-05 23:54:18 +0100

    Adde Template that extended from base

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -17,7 +17,14 @@ class HelloController extends AbstractController
     #[Route('/{limit<\d+>?3}', name:'app_index')]
     public function index(int $limit): Response
     {
-        return new Response(implode(',', array_slice($this->messages, 0, $limit)));
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'message' => implode(',', array_slice($this->messages, 0, $limit))
+            ]
+            );
+
+   
     }
 
     #[Route('/messages/{id<\d+>}',  name:'app_showOne')]
