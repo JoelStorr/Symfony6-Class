3cb1f9a27b8703587273680fc62a64c185adb7b6
Joel Storr <joel.storr@ewe.net>
2023-03-07 08:49:42 +0100

    Added Symfony Form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,23 +11,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-       $microPost1 = new MicroPost();
-       $microPost1->setTitle('Welcome to Germany!');
-       $microPost1->setText('Welcome to Germany!');
-       $microPost1->setCreated(new DateTime());
-       $manager->persist($microPost1); // Stages Data to be Added to Table
+        $microPost1 = new MicroPost();
+        $microPost1->setTitle('Welcome to Germany!');
+        $microPost1->setText('Welcome to Germany!');
+        $microPost1->setCreated(new DateTime());
+        $manager->persist($microPost1); // Stages Data to be Added to Table
 
-       $microPost2 = new MicroPost();
-       $microPost2->setTitle('Welcome to US!');
-       $microPost2->setText('Welcome to US!');
-       $microPost2->setCreated(new DateTime());
-       $manager->persist($microPost2); // Stages Data to be Added to Table
+        $microPost2 = new MicroPost();
+        $microPost2->setTitle('Welcome to US!');
+        $microPost2->setText('Welcome to US!');
+        $microPost2->setCreated(new DateTime());
+        $manager->persist($microPost2); // Stages Data to be Added to Table
 
-       $microPost3 = new MicroPost();
-       $microPost3->setTitle('Welcome to Poland!');
-       $microPost3->setText('Welcome to Poland!');
-       $microPost3->setCreated(new DateTime());
-       $manager->persist($microPost3); // Stages Data to be Added to Table
+        $microPost3 = new MicroPost();
+        $microPost3->setTitle('Welcome to Poland!');
+        $microPost3->setText('Welcome to Poland!');
+        $microPost3->setCreated(new DateTime());
+        $manager->persist($microPost3); // Stages Data to be Added to Table
 
 
 
