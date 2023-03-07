1f89e486322a6d18d10951120741f7bb638ff024
Joel Storr <joel.storr@ewe.net>
2023-03-07 09:49:04 +0100

    Submitted Form to Database

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
 {
@@ -33,7 +34,7 @@ class MicroPostController extends AbstractController
 
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
 
         $microPost = new MicroPost();
@@ -42,6 +43,19 @@ class MicroPostController extends AbstractController
             ->add('text')
             ->add('submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+
+            //TODO:Add a Flash Message
+            //TODO: Add Redirect
+
+
+        }
 
         return $this->renderForm(
             'task/add.html.twig',
