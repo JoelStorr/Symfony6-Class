da4093fa4b5241548418dae44d863c2b03105530
Joel Storr <joel.storr@ewe.net>
2023-03-09 14:43:14 +0100

    Added Dummy Users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,50 @@ namespace App\DataFixtures;
 
 use DateTime;
 use App\Entity\MicroPost;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    )
+    {
+        
+    }
     public function load(ObjectManager $manager): void
     {
+
+
+        # Add User
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                '123456789'
+            )
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('john@test.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                '123456789'
+            )
+        );
+        $manager->persist($user2);
+
+
+
+
+        # Add Dummy Posts
         $microPost1 = new MicroPost();
         $microPost1->setTitle('Welcome to Germany!');
         $microPost1->setText('Welcome to Germany!');
