f581a13e798470d4038ae76ed6c45f050828fc47
Joel Storr <joel.storr@ewe.net>
2023-03-12 21:04:55 +0100

    Added Like Functionality

---
+++ src/Controller/LikeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class LikeController extends AbstractController
+{
+    #[Route('/like/{id}', name: 'app_like')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function like(
+        MicroPost $post, 
+        MicroPostRepository $posts,
+        Request $request
+    ): Response {
+        $currentUser = $this->getUser();
+        $post->addLikedBy($currentUser);
+        $posts->save($post, true);
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    #[Route('/unlike/{id}', name: 'app_unlike')]
+    public function unlike(
+        MicroPost $post, 
+        MicroPostRepository $posts,
+        Request $request
+    ): Response {
+        $currentUser = $this->getUser();
+        $post->removeLikedBy($currentUser);
+        $posts->save($post, true);
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
